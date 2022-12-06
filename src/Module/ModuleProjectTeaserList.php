bac36151ba7d687fb47ee886f43827162c13bc1f
Lucas GEHIN <lucasgehin88@gmail.com>
2022-12-06 13:28:54 +0100

    feat: structure module project

---
+++ src/Module/ModuleProjectTeaserList.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace LucasGehin\ContaoEastBarsBundle\Module;
+
+use Contao\Module;
+use Contao\System;
+
+class ModuleProjectTeaserList extends Module
+{
+    /**
+     * Template
+     * @var string
+     */
+    protected $strTemplate = 'mod_eb_project_teaser_list';
+
+    public function generate()
+    {
+        $request = System::getContainer()->get('request_stack')->getCurrentRequest();
+
+        if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
+        {
+            $objTemplate = new \BackendTemplate('be_wildcard');
+            $objTemplate->wildcard = '### Module liste des derniers projets ###';
+            $objTemplate->title = $this->headline;
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->href = 'contao/main.php?do=themes&amp;table=tl_module&amp;act=edit&amp;id=' . $this->id;
+
+            return $objTemplate->parse();
+        }
+
+        return parent::generate();
+    }
+
+    protected function compile()
+    {
+
+    }
+}
--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,5 +2,7 @@
 
 $GLOBALS['FE_MOD']['eastBars'] = [
     'memberEastBars' => LucasGehin\ContaoEastBarsBundle\Module\ModuleMemberEastBars::class,
+    'projectTeaserList' => LucasGehin\ContaoEastBarsBundle\Module\ModuleProjectTeaserList::class,
+    'projectList' => LucasGehin\ContaoEastBarsBundle\Module\ModuleProjectList::class,
 ];
 
